199a406a80b2b591f49bbbda8184c77c38587835
Wiktor Bednarz <b.w.roboczy@gmail.com>
2019-04-11 17:49:23 +0200

    add imports and base constants

--- src/gameLogic/game.js
+++ src/gameLogic/game.js
@@ -1,7 +1,18 @@
 'use strict'
 // Imports
+const pf = require('pathfinding');
+const board = require('board.js');
 
 // Constants
+const baseLevel = 10;
+
+const initialState = () => ({
+  board: board.setState(board.initialState()),
+  nextLevel: baseLevel,
+  score: 0,
+  selectionStart: [],
+  selectionEnd: []
+});
 
 /*****************
   GAME MECHANICS
